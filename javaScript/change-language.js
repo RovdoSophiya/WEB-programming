ad059b33c6af52cb8207007f83c2bdeaa8be7762
Sophiya <rovdosofia@gmal.com>
2024-04-26 14:24:20 +0300

    Add russian language to index.html

--- javaScript/change-language.js
+++ javaScript/change-language.js
@@ -9,8 +9,10 @@ function loadTranslations(language) {
     })
     .catch((error) => console.error("Ошибка:", error));
 }
+
 function updateContent() {
-  document.getElementById("shoppingAnnotation").textContent = translations.shoppingAnnotation;
+  document.getElementById("shoppingAnnotation").textContent =
+    translations.shoppingAnnotation;
 
   document.getElementById("shop").textContent = translations.shop;
   document.getElementById("edit").textContent = translations.edit;
@@ -20,16 +22,47 @@ function updateContent() {
   document.getElementById("rus").textContent = translations.rus;
   document.getElementById("en").textContent = translations.en;
 
-  document.getElementById("signIn").textContent = translations.signIn;
-  document.getElementById("signUp").textContent = translations.signUp;
-
   document.getElementById("shopText").textContent = translations.shopText;
 
-  document.getElementById("greenMenuText1").textContent = translations.greenMenuText1;
-  document.getElementById("greenMenuText2").textContent = translations.greenMenuText2;
-  document.getElementById("greenMenuText3").textContent = translations.greenMenuText3;
+  document.getElementById("greenMenuText1").textContent =
+    translations.greenMenuText1;
+  document.getElementById("greenMenuText2").textContent =
+    translations.greenMenuText2;
+  document.getElementById("greenMenuText3").textContent =
+    translations.greenMenuText3;
 
   document.getElementById("pickBottles").textContent = translations.pickBottles;
+
+  document.getElementById("warmthDesert").textContent =
+    translations.warmthDesert;
+  document.getElementById("shopSets").textContent = translations.shopSets;
+
+  document.getElementById("meetDae").textContent = translations.meetDae;
+  document.getElementById("textDae").textContent = translations.textDae;
+  document.getElementById("learnMore").textContent = translations.learnMore;
+
+  document.getElementById("followingLink").textContent =
+    translations.followingLink;
+
+  document.getElementById("sustainability").textContent =
+    translations.sustainability;
+  document.getElementById("contactUs").textContent = translations.contactUs;
+  document.getElementById("FAQ").textContent = translations.FAQ;
+  document.getElementById("careers").textContent = translations.careers;
+  document.getElementById("privacyPolicy").textContent =
+    translations.privacyPolicy;
+  document.getElementById("CAPrivacyRights").textContent =
+    translations.CAPrivacyRights;
+  document.getElementById("termsOfUse").textContent = translations.termsOfUse;
+  document.getElementById("newsletter").textContent = translations.newsletter;
+
+  document.getElementById("firstEmailText").textContent =
+    translations.firstEmailText;
+  document.getElementById("subscribe").textContent = translations.subscribe;
+  document.getElementById("underEmailText").textContent =
+    translations.underEmailText;
+  document.getElementById("address").textContent = translations.address;
+  document.getElementById("rights").textContent = translations.rights;
 }
 
 const rusButton = document.getElementById("rus"); // Выбираем кнопку смены языка по идентификатору
@@ -37,11 +70,17 @@ const enButton = document.getElementById("en"); // Выбираем кнопку
 
 rusButton.addEventListener("click", () => {
   loadTranslations("rus");
+  localStorage.setItem("language", "rus");
 });
 
 enButton.addEventListener("click", () => {
   loadTranslations("en");
+  localStorage.setItem("language", "en");
 });
 
-
-loadTranslations("en"); // Загрузка английских переводов по умолчанию
+const savedLanguage = localStorage.getItem("language");
+if (savedLanguage) {
+  loadTranslations(savedLanguage);
+} else {
+  loadTranslations("en"); // Если язык не сохранен в localStorage, загрузим английский
+}
