950b20870f89b13867c0f3c21f6d6510175d1cbe
Sophiya <rovdosofia@gmal.com>
2024-05-10 11:21:34 +0300

    Add burger-menu

--- javaScript/change-theme.js
+++ javaScript/change-theme.js
@@ -1,11 +1,18 @@
+const lightThemeButton = document.getElementById("lightTheme");
+const darkThemeButton = document.getElementById("darkTheme");
+
 function setDarkTheme() {
   document.body.setAttribute("dark", "");
   localStorage.setItem("theme", "dark");
+  darkThemeButton.classList.add("dark-theme");
+  lightThemeButton.classList.remove("light-theme");
 }
 
 function setLightTheme() {
   document.body.removeAttribute("dark");
   localStorage.setItem("theme", "light");
+  lightThemeButton.classList.add("light-theme");
+  darkThemeButton.classList.remove("dark-theme");
 }
 
 document.getElementById("darkTheme").addEventListener("click", setDarkTheme);
